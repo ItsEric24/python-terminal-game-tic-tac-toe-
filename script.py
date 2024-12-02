8e150927b5743a8cfca7575200dc2d1d65aa0a82
Eric_24 <eric_24@Eric-24s-MacBook-Pro.local>
2024-12-02 20:24:44 +0300

    removed the second print statement when user starts the game

--- script.py
+++ script.py
@@ -21,7 +21,6 @@ game_board = [
 # * Game Logic:
 
 def display_board(board):
-    print("Welcome to the game once again!")
     for i in range(len(board)):
         print("  |  ".join(board[i]))
         if i < len(board) - 1:
